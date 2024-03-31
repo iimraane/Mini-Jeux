7a6ef95503e02edbdb3e18598c037945fc7c12ef
iimraane <zeleo789789@gmail.com>
2024-03-31 17:45:48 +0000

    Ajout d'une definition pour rejouer et gerer le texte

--- deflist_random.py
+++ deflist_random.py
@@ -10,4 +10,13 @@ def demander_nombre_random(prompt='> ', min_val=1):
             print("Ce n'est pas un nombre valide. Veuillez réessayer.")
             return demander_nombre_random()
     
-    
+def demander_action_random():
+    """Demander à l'utilisateur s'il veut rejouer ou revenir au menu."""
+    while True:
+        # Demander à l'utilisateur son choix et le mettre en minuscules
+        entree_utilisateur = input("Votre choix: ").lower()
+        # Vérifier si le choix de l'utilisateur est valide
+        if entree_utilisateur in ["oui", "non"]:
+            return entree_utilisateur  # Retourner le choix de l'utilisateur
+        else:
+            print("Ce n'est pas une entrée valide. Veuillez réessayer.")  # Message d'erreur
