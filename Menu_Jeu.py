8661eb5829e563867c82c7196d2e4ef1f312561c
iimraane <zeleo789789@gmail.com>
2024-04-01 15:12:40 +0000

    Rajout de 'money.py' dans le choix des jeux

--- Menu_Jeu.py
+++ Menu_Jeu.py
@@ -11,13 +11,17 @@ nom_fichier2 = "import_random.py"
 chemin_fichier2 = os.path.join(repertoire_script, nom_fichier2)
 current_user = os.getlogin()
 
+nom_fichier3 = "money.py"
+chemin_fichier3 = os.path.join(repertoire_script, nom_fichier3)
+
 print()
 print("Bienvenue dans le menu des jeu !")
 print(f"A quoi voulez-vous jouer aujourd'hui, {current_user} ?")
 print()
 print("Pour jouer a 'Pierre, Feuille, Ciseaux' taper 1")
 print("Pour jouer a 'C'est plus, C'est moins' taper 2")
-print("Pour quitter tapez 3")
+print("Pour jouer a 'Money' taper 3")
+print("Pour quitter tapez 4")
 print()
 
 while True:
@@ -30,4 +34,7 @@ while True:
         os.system(f"python \"{chemin_fichier2}\"")
     
     elif entree_utilisateur == 3:
+        os.system(f"python \"{chemin_fichier3}\"")
+
+    elif entree_utilisateur == 4:
         sys.exit()
